5cc4d16d1aafa29d0ca72d7a6d177335d9d2f06a
Michael Schaarschmidt <michael@schaaryworks.com>
2014-12-07 13:43:55 +0100

    initial commit

---
+++ migrate/sos_k_abint_tbl.sql
@@ -0,0 +1,21 @@
+create table sos_k_abint_tbl (
+	abint     varchar2(255),
+	aikz     varchar2(255),
+	ktxt     varchar2(255),
+	dtxt     varchar2(255),
+	ltxt     varchar2(255),
+	astat varchar2(255),
+	hrst varchar2(255),
+	part varchar2(255),
+	anzstg integer,
+	kzfaarray varchar2(255),
+	mag_laa varchar2(255),
+	sortkz1 varchar2(255),
+	anzstgmin integer,
+	sprache varchar2(255),
+	refabint varchar2(255),
+	efh varchar2(255),
+	astat_bund varchar2(255),
+	stufrm varchar2(255)
+)
+/
